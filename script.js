775f7068ed7731346611cd35cd475a47bc18551e
Michael Adeyeye <mikkygraphix14@gmail.com>
2024-11-27 02:08:08 +0100

    fifth commit

--- script.js
+++ script.js
@@ -197,6 +197,44 @@ const scrollTop = document.querySelector('.scroll-top');
 });
 
 
+$(document).ready(function () {
+    var owl = $(".owl-carousel").owlCarousel({
+        items: 1, 
+        loop: true,
+        dots: false, 
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        margin: 10, 
+        responsive: {
+            728: {
+                items: 2, 
+                margin: 18 
+            },
+            1024: {
+                items: 3, 
+                margin: 10 
+            }
+        }
+    });
+
+    // Custom dots click event to trigger the carousel change
+    $('.custom-dot').click(function () {
+        var index = $(this).data('index'); // Get me the index of the clicked dot
+        owl.trigger('to.owl.carousel', [index, 300]); // I want you to move to the corresponding dots
+    });
+
+    // Sync custom dots with the carousel's current state
+    owl.on('changed.owl.carousel', function (event) {
+        var currentIndex = event.item.index - event.relatedTarget._clones.length / 2; 
+        currentIndex = currentIndex % event.item.count; 
+        $('.custom-dot').removeClass('active'); 
+        $('.custom-dot').eq(currentIndex).addClass('active');
+    });
+});
+
+
 
 
 /////TESTIMONIAL
